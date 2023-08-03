1c8575e3b943409ad46b8f75e6015e3faf234325
manonab <manon.abel-coindoz@hostnfly.com>
2023-08-03 16:56:32 +0200

    added somes routes to delete, update ads and users

--- app/routes/ads/index.tsx
+++ app/routes/ads/index.tsx
@@ -1,12 +1,13 @@
 import { FieldPacket, RowDataPacket, ResultSetHeader } from "mysql2";
 import { Router, Request, Response } from "express";
 import pool from "../../database";
-import { verifyToken } from "../../middleware/verifyToken";
+import { CustomRequest, verifyToken } from "../../middleware/verifyToken";
 
 const adsRouter = Router();
 
-adsRouter.post("/ads", verifyToken, async (req: Request, res: Response) => {
+adsRouter.post("/ads", verifyToken, async (req: CustomRequest, res: Response) => {
   try {
+    const user_id = req.user?.user_id;
     const {
       title,
       description,
@@ -40,11 +41,12 @@ adsRouter.post("/ads", verifyToken, async (req: Request, res: Response) => {
       images && images.length > 0 ? images[0].url : "url_de_l_image_par_defaut.jpg";
 
     const query = `
-      INSERT INTO ads (title, description, price, category, thumbnail_url, location, seller_email, seller_name, seller_phone, is_vintage, brand, updated_at)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())
+      INSERT INTO ads (user_id,title, description, price, category, thumbnail_url, location, seller_email, seller_name, seller_phone, is_vintage, brand, updated_at)
+      VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())
     `;
 
     const [adResult]: [ResultSetHeader, FieldPacket[]] = await pool.execute(query, [
+      user_id,
       title,
       description,
       price,
@@ -75,7 +77,7 @@ adsRouter.post("/ads", verifyToken, async (req: Request, res: Response) => {
   }
 });
 
-adsRouter.get("/ads", async (_req: Request, res: Response) => {
+adsRouter.get("/ads", async (req: Request, res: Response) => {
   try {
     const query = `
       SELECT ads.*, images.url as thumbnail_url
@@ -93,6 +95,7 @@ adsRouter.get("/ads", async (_req: Request, res: Response) => {
       seller_phone: ad.seller_phone,
       seller_name: ad.seller_name,
       brand: ad.brand,
+      user_id: ad.user_id,
       location: ad.location,
       seller_email: ad.seller_email,
       description: ad.description,
@@ -111,6 +114,45 @@ adsRouter.get("/ads", async (_req: Request, res: Response) => {
   }
 });
 
+adsRouter.get("/my_ads", verifyToken, async (req: CustomRequest, res: Response) => {
+  try {
+    const user_id = req.user?.user_id;
+
+    const query = `
+      SELECT ads.*, images.url as thumbnail_url
+      FROM ads
+      LEFT JOIN images ON ads.id = images.ad_id
+      WHERE ads.user_id = ?
+    `;
+
+    const connection = await pool.getConnection();
+    const [adsWithThumbnails] = await connection.query(query, [user_id]);
+    connection.release();
+
+    const ads = (adsWithThumbnails as RowDataPacket[]).map((ad) => ({
+      id: ad.id,
+      is_vintage: ad.is_vintage,
+      seller_phone: ad.seller_phone,
+      seller_name: ad.seller_name,
+      brand: ad.brand,
+      user_id: ad.user_id,
+      location: ad.location,
+      seller_email: ad.seller_email,
+      description: ad.description,
+      price: ad.price,
+      state: ad.state,
+      created_at: ad.created_at,
+      category: ad.category,
+      time_ago: getTimeAgo(ad.created_at),
+      thumbnail_url: ad.thumbnail_url || "url_de_l_image_par_defaut.jpg",
+    }));
+
+    res.status(200).json(ads);
+  } catch (error) {
+    console.error("Error while fetching user ads:", error);
+    res.status(500).json({ message: "Server error while fetching user ads." });
+  }
+});
 adsRouter.get("/ads/:id", async (req: Request, res: Response) => {
   const adId = req.params.id;
 
@@ -142,6 +184,107 @@ adsRouter.get("/ads/:id", async (req: Request, res: Response) => {
   }
 });
 
+adsRouter.put(
+  "/update_ads/:id",
+  verifyToken,
+  async (req: CustomRequest, res: Response) => {
+    const userId = req.user?.user_id;
+    const adId = req.params.id;
+
+    try {
+      const [adResult]: [RowDataPacket[], FieldPacket[]] = await pool.execute(
+        "SELECT * FROM ads WHERE id = ? AND user_id = ?",
+        [adId, userId],
+      );
+
+      if (adResult.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "Ad not found or you are not authorized to update it" });
+      }
+
+      const { title, description, price, category, images, location, is_vintage, brand } =
+        req.body;
+
+      if (
+        !title ||
+        !description ||
+        !price ||
+        !category ||
+        !location ||
+        !is_vintage ||
+        !brand
+      ) {
+        return res
+          .status(400)
+          .json({ message: "Please provide all the required values." });
+      }
+
+      // Mettre à jour l'annonce dans la base de données
+      const thumbnailUrl =
+        images && images.length > 0 ? images[0].url : "url_de_l_image_par_defaut.jpg";
+      const updateQuery = `
+      UPDATE ads
+      SET title = ?, description = ?, price = ?, category = ?, thumbnail_url = ?, location = ?, is_vintage = ?, brand = ?, updated_at = NOW()
+      WHERE id = ? AND user_id = ?
+    `;
+      await pool.execute(updateQuery, [
+        title,
+        description,
+        price,
+        category,
+        thumbnailUrl,
+        location,
+        is_vintage,
+        brand,
+        adId,
+        userId,
+      ]);
+
+      await pool.execute("DELETE FROM images WHERE ad_id = ?", [adId]);
+
+      if (images && Array.isArray(images)) {
+        const imageQuery = "INSERT INTO images (ad_id, url) VALUES (?, ?)";
+        for (const imageUrl of images) {
+          const imageValues = [adId, imageUrl.url];
+          await pool.execute(imageQuery, imageValues);
+        }
+      }
+
+      res.status(200).json({ message: "Ad updated successfully." });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+);
+
+adsRouter.delete("/ads/:id", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const adId = req.params.id;
+
+    // Vérifiez si l'annonce avec l'ID donné existe dans la base de données
+    const [ads]: [RowDataPacket[], FieldPacket[]] = await pool.execute(
+      "SELECT * FROM ads WHERE id = ?",
+      [adId],
+    );
+
+    if (ads.length === 0) {
+      return res.status(404).json({ message: "Ad not found" });
+    }
+
+    // Vérifiez si l'utilisateur connecté est le propriétaire de l'annonce (ou a les autorisations appropriées pour supprimer l'annonce)
+    // Vous pouvez implémenter la logique pour vérifier cela en fonction de votre système d'authentification et des autorisations des utilisateurs
+
+    // Supprimez l'annonce de la base de données
+    await pool.execute("DELETE FROM ads WHERE id = ?", [adId]);
+
+    res.status(200).json({ message: "Ad deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting the ad" });
+  }
+});
 function getTimeAgo(timestamp: string): string {
   const currentTime = Date.now();
   const adTime = Date.parse(timestamp);
--- app/routes/users/index.ts
+++ app/routes/users/index.ts
@@ -2,6 +2,7 @@ import { CustomRequest, verifyToken } from "../../middleware/verifyToken";
 import { Router, Response } from "express";
 import { FieldPacket, RowDataPacket } from "mysql2";
 import pool from "../../database";
+import bcrypt from "bcrypt";
 
 const userRouter = Router();
 
@@ -24,6 +25,51 @@ userRouter.get("/me", verifyToken, async (req: CustomRequest, res: Response) => 
        console.error(error);
        res.status(500).json({ message: "Internal server error" });
      }
-   });
+});
+
+userRouter.put("/profile", verifyToken, async (req: CustomRequest, res: Response) => {
+     const userId = req.user?.user_id;
+     const { username, email, password } = req.body;
+
+     if (!username && !email && !password) {
+          return res.status(400).json({ message: "No updates provided" });
+     }
+  try {
+    const [userResult]: [RowDataPacket[], FieldPacket[]] = await pool.execute(
+      "SELECT * FROM users WHERE user_id = ?",
+      [userId],
+    );
+
+    if (userResult.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const [existingUser]: [RowDataPacket[], FieldPacket[]] = await pool.execute(
+      "SELECT * FROM users WHERE email = ? AND user_id <> ?",
+      [email, userId],
+    );
+ 
+    if (existingUser.length > 0) {
+      return res.status(409).json({ message: "Email already in use by another user" });
+    }
+    if (userId !== userResult[0].user_id) {
+     return res.status(403).json({ message: "Access denied. You can only update your own account." });
+   }
+    let hashedPassword = userResult[0].password; // conserver le mot de passe actuel s'il n'est pas modifié
+    if (password) {
+      hashedPassword = await bcrypt.hash(password, 10);
+    }
+
+    await pool.execute(
+      "UPDATE users SET username = ?, email = ?, password = ? WHERE user_id = ?",
+      [username, email, hashedPassword, userId],
+    );
+
+    res.status(200).json({ message: "Account updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+})
 
 export default userRouter;
