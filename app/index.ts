fdd6917c20e4576b3b2de58e7355dfc5a05b4f12
manonab <manon.abel-coindoz@hostnfly.com>
2023-10-26 16:50:00 +0200

    started to add body parser and cors enabled

--- app/index.ts
+++ app/index.ts
@@ -7,9 +7,8 @@ import userRouter from './routes/users';
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
 app.use(cors());
-
+app.use(express.urlencoded({ extended: true }));
 app.use(adsRouter);
 app.use(authRouter);
 app.use(userRouter);
@@ -17,3 +16,9 @@ app.use(userRouter);
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
+
+app.use(
+  cors({
+    origin: "https://flourishing-nasturtium-1e40fd.netlify.app",
+  }),
+);
