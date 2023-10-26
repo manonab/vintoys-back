971bdfb6b06b31914bea189870e0762f8e51de6d
manonab <manon.abel-coindoz@hostnfly.com>
2023-10-26 16:06:09 +0200

    trying to add post add with s3 bucket

--- app/index.ts
+++ app/index.ts
@@ -7,9 +7,8 @@ import userRouter from './routes/users';
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
 app.use(cors());
-
+app.use(express.json({ limit: "50mb" }));
 app.use(adsRouter);
 app.use(authRouter);
 app.use(userRouter);
--- app/utils/aws.tsx
+++ app/utils/aws.tsx
@@ -1,5 +1,3 @@
-const aws = require('aws-sdk');
-
 export const awsConfig = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
