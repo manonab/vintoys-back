498270ddb4f04b5f7cfbd04ffbc19f8f7f00a4a6
manonab <manon.abel-coindoz@hostnfly.com>
2023-10-26 15:05:23 +0200

    removed node modules to reinstall it

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,10 @@
-import bodyParser from "body-parser";
 import express from "express";
+import bodyParser from "body-parser";
 import adsRouter from "./routes/ads";
 import cors from "cors";
 import authRouter from "./routes/auth";
 import userRouter from "./routes/users";
+
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(bodyParser.json({ limit: "100mb" }));
