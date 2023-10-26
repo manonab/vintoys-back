91d508ec3c5bdfc892d033468261e3b43a31c9ca
manonab <manon.abel-coindoz@hostnfly.com>
2023-10-26 17:27:19 +0200

    added body parser

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
 import adsRouter from "./routes/ads";
 import cors from "cors";
 import authRouter from "./routes/auth";
 import userRouter from "./routes/users";
 const app = express();
 const port = process.env.PORT || 3001;
-
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(adsRouter);
