a23762beb0df7b1574225a834d025dd57cb8c339
manonab <manon.abel-coindoz@hostnfly.com>
2023-10-27 10:40:02 +0200

    trying to fix cors pb

--- app/index.ts
+++ app/index.ts
@@ -4,9 +4,23 @@ import adsRouter from "./routes/ads";
 import cors from "cors";
 import authRouter from "./routes/auth";
 import userRouter from "./routes/users";
+
 const app = express();
 const port = process.env.PORT || 3001;
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://benevolent-pixie-c8f6ac.netlify.app",
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+app.use(cors());
+
 app.use(adsRouter);
 app.use(authRouter);
 app.use(userRouter);
@@ -14,9 +28,3 @@ app.use(userRouter);
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
-
-
-app.use(cors());
-app.use(bodyParser.json({ limit: "100mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
-
