a58e3bfae026fa8ae2ab2dbbf9ea56240f3ea569
manonab <manon.abel-coindoz@hostnfly.com>
2023-10-28 14:24:45 +0200

    added utils file and rebase

---
+++ app/utils/aws.tsx
@@ -0,0 +1,7 @@
+const aws = require('aws-sdk');
+
+export const awsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+};
